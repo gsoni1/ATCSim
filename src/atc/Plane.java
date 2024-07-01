ad3b32b50c02939478ab6804f1f65f11899089fb
Gautam Soni <111721922+gsoni1@users.noreply.github.com>
2024-07-01 11:51:06 -0400

    added most field variables

--- src/atc/Plane.java
+++ src/atc/Plane.java
@@ -1,5 +1,23 @@
 package atc;
 
 public class Plane {
-    private String manufacturer;
+    private String planeModel; // B747
+    private int planeAltitude; // 39025 Ft
+    private int planeSpeed; // 498 Kts
+    private Double planeCoordinatesX;
+    private Double planeCoordinatesY;
+    private Double[] planeCoordinates;
+    private String planeCallSign;
+    private String planeFlightNumber;
+    private String planeIATATypeCode;
+    private String planeRegistration; 
+    private String planeAirline; // Qantas
+    private String planeOrigin; // FRA
+    private String planeDestination; // DXB
+    private Double planeArrivalTime; // 7.00 (EST)
+    private Double planeDepartureTime; // 14.00 (EST)
+    private int planeNumberOfPassengers; // 200 passengers
+    private int planeFlightTime; // 14 hrs
+    private String planeStatus; // ground,air,off
+    private String planeFlightStatus; // delayed,on time,
 }
