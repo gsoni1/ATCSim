acfd947637e2b192a0311797abbf634f5b8dd40c
Gautam Soni <111721922+gsoni1@users.noreply.github.com>
2024-07-08 03:45:36 -0400

    added setup method for jUnit testing
    note: this class and its original class are not done

--- src/atc/AirspaceTest.java
+++ src/atc/AirspaceTest.java
@@ -1,16 +1,50 @@
 package atc;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 public class AirspaceTest {
     // fields
-    private Airspace londonHeathrow = new Airspace();
-    Plane AI101 = new Plane("AI101", "Mid air");
+    private static Airspace londonHeathrow;
+    private static Plane AI101;
+
+    /**
+     * SetUp method, initializes all the instance variables
+     */
+    @BeforeAll
+    public static void setUp() {
+        londonHeathrow = new Airspace();
+        AI101 = new Plane("AI101", "Mid air");
+    }
+    /**
+     * Tests the addToAirspace method using a plane that exists within the airspace
+     */
     @Test
     public void testAddToAirspace() {
         londonHeathrow.addToAirspace(AI101);
         assertEquals(AI101, londonHeathrow.getPlaneInAirspace(AI101));
+    }
+
+    /**
+     * Tests the removeFromAirspace method using a plane that exists in the airspace, first by adding then removing the
+     * plane
+     */
+    @Test
+    public void testRemoveFromAirspace() {
+        londonHeathrow.addToAirspace(AI101);
+        assertEquals(AI101, londonHeathrow.getPlaneInAirspace(AI101));
+        londonHeathrow.removeFromAirspace(AI101);
+        assertNotEquals(AI101, londonHeathrow.getPlaneInAirspace(AI101));
+    }
+
+    /**
+     * Tests the getPlaneInAirspace method using a plane that wasn't added to the airspace
+     * (this covers the case of a plane being parked/off/in maintainance/not in this airport's airspace)
+     */
+    @Test
+    public void testGetPlaneInAirspace() {
+        assertNotEquals(AI101, londonHeathrow.getPlaneInAirspace(AI101));
     }
 
 }
