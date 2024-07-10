bd841ae91f714e598f59b2ff49ba2e6603fdb798
arihitaD <arihita.dirghangi@gmail.com>
2024-07-10 17:02:51 -0400

    Plane Class

--- src/atc/Plane.java
+++ src/atc/Plane.java
@@ -32,15 +32,181 @@ public class Plane {
      */
     public Plane(String callSign, String planeStatus) {
         planeCallSign = callSign;
+        planeFlightStatus = planeStatus;
     }
 
-    public String getStatus() {
+    /**
+     * Getter method for variable planeFlightStatus
+     * @return String indicating whether flight is delayed, on time, etc.
+     */
+    public String getPlaneFlightStatus() {
         return planeFlightStatus;
     }
 
+    /**
+     * Getter method for variable planeCallSign
+     * @return String name of the flight called
+     */
     public String getPlaneCallSign() {
         return planeCallSign;
     }
 
-    public String getPlaneModel() {return planeModel; }
+    /**
+     * Getter method for variable planeModel
+     * @return Plane Model type
+     */
+    public String getPlaneModel()
+    {
+        return planeModel;
+    }
+
+    /**
+     * Getter method for variable planeAltitude
+     * @return altitude of the plane in feet
+     */
+    public int getPlaneAltitude()
+    {
+        return planeAltitude;
+    }
+
+    /**
+     * Getter method for variable planeSpeed
+     * @return speed of the plane in Kts
+     */
+    public int getPlaneSpeed()
+    {
+        return planeSpeed;
+    }
+
+    /**
+     * Getter method for variable planeCoordinatesX
+     * @return x coordinates of the plane
+     */
+    public Double getPlaneCoordinatesX()
+    {
+        return planeCoordinatesX;
+    }
+
+    /**
+     * Getter method for variable planeCoordinatesY
+     * @return y coordinates of the plane
+     */
+    public Double getPlaneCoordinatesY()
+    {
+        return planeCoordinatesY;
+    }
+
+    /**
+     * Getter method for variable planeCoordinates
+     * @return array with x and y coordinates as [planeCoordinatesX, planeCoordinatesY]
+     */
+    public Double getPlaneCoordinates()
+    {
+        return planeCoordinates[];
+    }
+
+    /**
+     * Getter method for variable planeFlightNumber
+     * @return string of the flight number
+     */
+    public String getPlaneFlightNumber()
+    {
+        return planeFlightNumber;
+    }
+
+    /**
+     * Getter method for variable planeIATATypeCode
+     * @return the IATA Type Code as a string
+     */
+    public String planeIATATypeCode()
+    {
+        return planeIATATypeCode;
+    }
+
+    /**
+     * Getter method for variable planeRegistration
+     * @return the plane registration code as a string
+     */
+    public String planeRegistration()
+    {
+        return planeRegistration;
+    }
+
+    /**
+     * Getter method for variable planeAirline
+     * @return the plane airline as a string
+     */
+    public string getPlaneAirline()
+    {
+        return planeAirline;
+    }
+
+    /**
+     * Getter method for the variable planeOrigin
+     * @return the plane origin as a string
+     */
+    public string getPlaneOrigin()
+    {
+        return planeOrigin;
+    }
+
+    /**
+     * Getter method for variable planeDestination
+     * @return the destination of the plane
+     */
+    public string getPlaneDestination()
+    {
+        return planeDestination:
+    }
+
+    /**
+     * Getter method for variable planeArrivalTime
+     * @return the arrival time of the plane
+     */
+    public Double getPlaneArrivalTime()
+    {
+        return planeArrivalTime;
+    }
+
+    /**
+     * Getter method for variable planeDepartureTime
+     * @return the departure time of the plane with the time zone
+     */
+    public Double getPlaneDepartureTime()
+    {
+        return planeDepartureTime;
+    }
+
+    /**
+     * Getter method for variable planeNumberOfPassengers
+     * @return the number of passengers expected/ onboarded on flight
+     */
+    public int getPlaneNumberOfPassengers()
+    {
+        return planeNumberOfPassengers;
+    }
+
+    /**
+     * Getter method for variable planeFlightTime
+     * @return the expected duration of flight
+     */
+    public int getPlaneFlightTime()
+    {
+        return planeFlightTime;
+    }
+
+    /**
+     * Getter method for variable planeStatus
+     * @return information about whether flight is at gate/taxxing/taking off/mid air/landing/arrived at gate
+     */
+    public string getPlaneStatus()
+    {
+        return planeStatus;
+    }
+
+
+
+
+
+
 }
