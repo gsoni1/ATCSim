f2dde938e94ad9c70a34ac92e1b279627e52e450
Gautam Soni <111721922+gsoni1@users.noreply.github.com>
2024-07-12 17:57:57 -0400

    added the AirSpaces interface and modified the Airspace class to implement it and define its methods

---
+++ src/main/java/atc/atcsim/AirSpaces.java
@@ -0,0 +1,18 @@
+package atc.atcsim;
+
+/**
+ * AirSpaces interface, declares all the methods for the Airspace class.
+ *
+ * @author Gautam Soni
+ * @version 7/12/2024
+ */
+public interface AirSpaces {
+    // Method Declarations
+    public void addToAirspace(Plane p);
+    public void removeFromAirspace(Plane p);
+    public Plane getPlaneInAirspace(Plane p);
+    public void closeAirspace();
+    public void openAirspace();
+    public void clearAirspace();
+    public int getAirspaceCapacity();
+}
--- src/main/java/atc/atcsim/Airspace.java
+++ src/main/java/atc/atcsim/Airspace.java
@@ -2,13 +2,13 @@ package atc.atcsim;
 import java.util.HashMap;
 
 /**
- * Airspace class, implements a hashmap to store planes in a given airspace. Provides methods to modify the contents
+ * Airspace class, implements the AirSpaces interface and defines all of its method, uses a hashmap to store planes in a given airspace. Provides methods to modify the contents
  * of the hashmap.
  *
  * @author Gautam Soni
  * @version 7/10/2024
  */
-public class Airspace {
+public class Airspace implements AirSpaces {
     // Fields
     private HashMap<String, Plane> airspace; // hashmap that stores all the planes
     private boolean airspaceOpen; // boolean to hold the status of whether the airspace is open or not
@@ -25,6 +25,7 @@ public class Airspace {
      * Adds the specified plane to the airspace if the airspace is open
      * @param p Plane to be added
      */
+    @Override
     public void addToAirspace(Plane p) {
         if (airspaceOpen) {
             airspace.put(p.getPlaneCallSign(), p);
@@ -38,6 +39,7 @@ public class Airspace {
      * Removes the specified plane from the airspace
      * @param p Plane to be removed
      */
+    @Override
     public void removeFromAirspace(Plane p) {
         if (p.getPlaneCallSign() != null) {
             airspace.remove(p.getPlaneCallSign());
@@ -49,17 +51,40 @@ public class Airspace {
      * @param p Plane to search for in airspace
      * @return Plane in the airspace
      */
+    @Override
     public Plane getPlaneInAirspace(Plane p) {
         return airspace.get(p.getPlaneCallSign());
     }
 
+    /**
+     * Closes the airspace by marking the airspaceOpen variable as false.
+     */
+    @Override
     public void closeAirspace() {
         airspaceOpen = false;
     }
 
+    /**
+     * Opens the airspace by marking the airspaceOpen variable as true;
+     */
+    @Override
+    public void openAirspace() {
+        airspaceOpen = true;
+    }
+
+    /**
+     * Clears the airspace by removing all objects in the airspace hashmap.
+     */
+    @Override
     public void clearAirspace() {
         airspace.clear();
     }
+
+    /**
+     * Getter method for the airspace capacity
+     * @return Number of planes currently in the airspace
+     */
+    @Override
     public int getAirspaceCapacity() {
         return airspace.size();
     }
