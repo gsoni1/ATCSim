53375610651ac89a608512949e37d161a27041a3
arihitaD <arihita.dirghangi@gmail.com>
2024-07-18 13:45:10 -0400

    Constructor Documentation of Plane Class Completed

--- src/main/java/atc/atcsim/AtcController.java
+++ src/main/java/atc/atcsim/AtcController.java
@@ -8,7 +8,7 @@ package atc.atcsim;
 public class AtcController {
     public AtcController() {
         AtcControllerGUI gui = new AtcControllerGUI();
-        Plane EK71 = new Plane("EK61", "midair"); // in the final version we can load chunks of
+        Plane EK71 = new Plane("EK61", "midair", 39025,498,"EK61", "B77W","A6- EQB", "Qantas", "FRA", "DXB", 7.00, 14.00, 200, 14,"taking off", "delayed"); // in the final version we can load chunks of
         // runways and planes and etc for usage, there can be multiple profiles, for example by airport like jfk,lax,dxb
         Airspace jfk = new Airspace();
         Runway R27 = new Runway("twenty seven right", jfk);
