d64d2c46ff73a875f3897532ce1dc2f0f43ce32a
arihitaD <arihita.dirghangi@gmail.com>
2024-07-25 12:27:56 -0400

    Terminal Class methods made

--- src/main/java/atc/atcsim/Terminal.java
+++ src/main/java/atc/atcsim/Terminal.java
@@ -1,4 +1,5 @@
 package atc.atcsim;
+import java.util.*;
 
 
 /**
@@ -11,4 +12,41 @@ package atc.atcsim;
 public class Terminal {
 
     ArrayList<Plane> gates = new ArrayList<Plane>();
+
+    //methods to add, remove, search objects from the ArrayList
+
+    /**
+     * Method that takes a plane object and adds it to the gate
+     * @param p is the object to be added
+     */
+    public void addGate(Plane p)
+    {
+        gates.add(p);
+    }
+
+    /**
+     * Method that remove's the gate for a cancelled or postponed flight
+     * @param p is the plane object being removed
+     */
+    public void removeGate(Plane p)
+    {
+        gates.remove(p);
+    }
+
+    /**
+     * Method to check if plane object exists in the Gates Arraylist
+     * @param p is the plane object being searched for
+     * @return boolean indicating whether the object has a Gate or not
+     */
+    public boolean containsGate(Plane p)
+    {
+        for(int i = 0; i < gates.size(); i++)
+        {
+            if(gates.get(i) == p)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
